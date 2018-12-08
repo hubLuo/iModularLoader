6e1ee532841dfe02d607ddefaa45a2ac4e593200
hubLuo <hubluo@126.com>
2018-12-09 01:02:05 +0800

    3.seajs：3-生命周期第二版 （1）主模块和子模块都标记周期状态（2）细化子模块加载状态：fetched和saved（3）递归load加载子模块依赖

--- 3.seajs生命周期第2版细化/b.js
+++ 3.seajs生命周期第2版细化/b.js
@@ -2,8 +2,8 @@
 * @Author: Max
 * @Date:   2018-12-03 22:16:22
 * @Last Modified by:   Max
-* @Last Modified time: 2018-12-04 18:50:04
+* @Last Modified time: 2018-12-06 00:48:50
 */
 define(function(require, exports, module) {
-  exports.name = "LQ";
+  exports.name = "LQ";  //接口对象
 });
--- js/iSea.js
+++ js/iSea.js
@@ -21,6 +21,14 @@
         getMainModuleId:function(){
             return this.cwd+"_use_"+this.cid();
         },
+        //类型判断
+        isArray:function(obj){
+            return toString.call(obj) === "[object Array]";
+        },
+        isFunction:function(obj){
+            return toString.call(obj) === "[object Function]";
+        },
+        //接口
         use:function(ids, callback){
             Module.preload(function(){
                 Module.use(ids, callback, seajs.getMainModuleId() );   //虚拟的根目录
@@ -28,26 +36,30 @@
         },
         define:function(factory){
             Module.define(factory);
+        },
+        //异步脚本加载
+        request:function( url, callback ){
+            var node = document.createElement("script");
+            node.src = url;
+            document.body.appendChild(node);
+            node.onload = function(){
+                node.onload = null;
+                document.body.removeChild(node);   //定义a模块  callback怎么获取
+                callback();
+            }
         }
     };
     //模块缓存
     var cacheMods = {},moduleFactory;
     //定义模块的生命周期
     var status = {
-        FETCHED: 1,   //获取模块的uri
+        FETCHED: 1,   //获取依赖模块
         SAVED: 2,    // 元数据存储在缓存中
         LOADING: 3,   //加载模块的依赖项
-        LOADED: 4,   //准备执行依赖项模块
+        LOADED: 4,   //执行主模块回调从而触发执行依赖项模块回调
         EXECUTING: 5,  //正在执行  加载模块
         EXECUTED: 6,    // 返回接口对象
     };
-    //类型判断
-    var isArray = function(obj){
-        return toString.call(obj) === "[object Array]";
-    }
-    var isFunction = function(obj){
-        return toString.call(obj) === "[object Function]";
-    }
 
     //模块构造函数
     var Module=function(uri, deps){
@@ -55,7 +67,8 @@
         this.deps = deps||[];
         this.exports = null;   //接口对象
         this.status = 0;
-        this._waitings = {};  //我有几个依赖项
+        this._waitings = {};  //记录当前模块所对应主模块，该主模块等待执行回调。
+        this._remain = 0;   // 当前（主）模块所依赖的但未完成加载的子模块数量。
     };
     //1.定义接口：定义模块，注入使用模块回调
     ~function(Module){
@@ -68,9 +81,10 @@
         //使用模块接口
         Module.use = function( deps, callback, uri ){
             //1.注册/读取主模块
-            var mod = Module.get(uri, isArray(deps) ? deps: [deps] );
+            var mod = Module.get(uri, seajs.isArray(deps) ? deps: [deps] );
             //2.定义依赖项模块都加载完毕时所要执行的回调
             mod.callback = function(){
+                mod.status = status.EXECUTING;  //5
                 var exports = [];   //所以依赖项模块的接口对象
                 var uris = mod.resolve(); //获取依赖模块的绝对路径
                 for(var i=0; i<uris.length; i++ ){
@@ -78,6 +92,7 @@
                 }
                 if(callback){
                     callback.apply(root, exports);
+                    mod.status = status.EXECUTED;  //5
                 }
             }
             //3.开启运行模块的生命周期
@@ -87,7 +102,21 @@
         Module.define = function(factory){
             //Module.prototype.factory = factory;//引用地址存储回调地址
             //值存储回调地址
-            moduleFactory=factory;
+            //moduleFactory=factory;
+            var deps;
+            if(seajs.isFunction(factory)){
+                //调用toString方法，正则解析依赖项
+                //本版本简化，默认给空数组
+                deps = [];   //["./c,"./d"]
+            }
+            //存储当前模块的信息
+            var meta = {
+                id:"",
+                uri:"",
+                deps:deps,
+                factory:factory
+            }
+            moduleFactory = meta;
         }
 
         //注册或读取模块：主模块和子（依赖）模块
@@ -104,41 +133,80 @@
             var mod = this;   // 携带了主模块中deps 依赖
             mod.status = status.LOADING;
             var uris = mod.resolve(); //获取依赖模块的绝对路径
-            var len = uris.length;
-            var m;
+            var len =mod._remain= uris.length,requestCache = {};
             for(var i=0; i<len; i++ ){
-                m = Module.get(uris[i]); //主模块下的子模块存储在缓存中
-                if(m.status < status.LOADING ){
-                    m._waitings[uris[i]] = m._waitings[uris[i]] || 0;
+                var m = Module.get(uris[i]); //在缓存中注册/读取主模块依赖的子模块
+                if(m.status < status.FETCHED){
+                    m.fetch(requestCache);
+                }
+                if(m.status < status.LOADED ){
+                    //记录当前模块所对应主模块，该主模块等待执行回调。
+                    m._waitings[mod.uri] = m._waitings[mod.uri] || 1;
+                }else{
+                    mod._remain--;
                 }
             }
-            //准备执行依赖项模块
-            mod.onload();
+            if(mod._remain == 0){
+                //所有依赖加载完成后，执行onload
+                mod.onload();
+            }else{
+                //执行加载依赖项
+                for( var uri in requestCache){
+                    requestCache[uri]();
+                }
+            };
         }
+        //生命周期fetched：加载依赖模块
+        Module.prototype.fetch = function(requestCache){
+            var mod = this;  //a b
+            mod.status = status.FETCHED;
+            var uri = mod.uri;
+            requestCache[uri] = sendRequest;   //发送请求  注入script
 
-        //生命周期loaded：执行子模块define，获取子模块向外提供接口的回调；
+            function sendRequest(){
+                seajs.request(uri, onRequest);
+            }
+
+            function onRequest(){    //当前模块的  deps 依赖项
+                if(moduleFactory){
+                    mod.save(uri,  moduleFactory);   //更改数据
+                }
+                mod.load();    //递归   根目录下的依赖项(a b)   (a b)是否还有依赖项  deps
+            }
+        };
+        //生命周期saved：修改模块信息
+        Module.prototype.save = function(uri, meta ){
+            var mod =  Module.get(uri);
+            mod.id = uri;
+            mod.deps = meta.deps || [];
+            mod.factory = meta.factory;
+            mod.status = status.SAVED;
+        };
+        //生命周期loaded：执行主模块回调
         Module.prototype.onload = function(){
             var mod = this;
-            var uris = mod.resolve(); //获取主模块下依赖模块的绝对路径获
-            var len = uris.length,loadedCount=0;
+            var uris = mod.resolve(); //获取依赖模块的绝对路径
+            var len = uris.length;
             mod.status = status.LOADED;
-            for(var i=0; i<len; i++ ){
-                (function(j){
-                    var node = document.createElement("script");
-                    node.src = uris[j];
-                    document.body.appendChild(node);
-                    node.onload = function(){
-                        //生命周期loaded：执行子模块define，获取子模块向外提供接口的回调；
-                        loadedCount++;
-                        //记录每个子模块回调factory
-                        cacheMods[uris[j]].factory=moduleFactory;
-                        if(mod.callback&&loadedCount==len){
-                            mod.callback();
-                        }
-                    }
-                })(i);
-            };
+            if(mod.callback){
+                //子模块中没有callback回调，它只存在于主模块中
+                mod.callback();
+            }
+
+            //伪递归
+            _waitings = mod._waitings;
+            var uri, m ;
+            for( uri in _waitings){
+                //获得该子模块对应的主模块
+                m = cacheMods[uri];
+                //主模块待加载依赖数减一
+                m._remain -= _waitings[uri];
+                //加载完毕，执行主模块回调mod.callback()
+                if(m._remain == 0){ m.onload()};   //依赖项模块数据已经更新完毕 deps   factory
+            }
+
         };
+
         //获取主模块下依赖模块的绝对路径
         Module.prototype.resolve = function(refresh){
             var mod = this,ids = mod.deps,refresh=refresh||false;
@@ -183,7 +251,7 @@
             }
 
             var factory = mod.factory;
-            var exports = isFunction(factory) ? factory(require, mod.exports = {}, mod) : factory;
+            var exports = seajs.isFunction(factory) ? factory(require, mod.exports = {}, mod) : factory;
 
             if(exports === undefined){
                 //当回调不返回值时
