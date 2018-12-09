81cebeac85876e6144425c1d86f859c282b09349
hubLuo <hubluo@126.com>
2018-12-10 00:00:24 +0800

    4.requirejs：1-架构初探 （1）子模块全部加载完成判断（2）子模块依赖的递归加载

---
+++ 1.requirejs架构初探/a.js
@@ -0,0 +1,17 @@
+/*
+* @Author: Max
+* @Date:   2018-12-02 21:43:24
+* @Last Modified by:   Max
+* @Last Modified time: 2018-12-02 22:25:24
+*/
+define("a",[], function(){   //依赖   先加载依赖
+  var name ="max";
+  function work(){
+
+}
+  //逻辑处理
+ return {   //接口对象
+   name: name,
+   work: work
+  }
+});   
---
+++ 1.requirejs架构初探/b.js
@@ -0,0 +1,18 @@
+/*
+* @Author: Max
+* @Date:   2018-12-02 21:43:31
+* @Last Modified by:   Max
+* @Last Modified time: 2018-12-02 22:25:23
+*/
+define("b",[], function(){
+ //逻辑处理
+  var name ="star";
+  function work(){
+
+}
+ return {   //接口对象
+   name: name,
+   work: work
+  }
+}); 
+
